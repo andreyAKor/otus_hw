5c40cc7fae57bfd3634447422981d3b923fcc0ed
andrey.a.kor <info@andreykor.com>
2020-03-23 17:29:12 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,47 @@
 package hw03_frequency_analysis //nolint:golint,stylecheck
 
-func Top10(_ string) []string {
-	// Place your code here
-	return nil
+import (
+	"regexp"
+	"sort"
+)
+
+const topSize int = 10
+
+type wordStats struct {
+	word  string
+	count int
+}
+
+func Top10(s string) []string {
+	if len(s) == 0 {
+		return nil
+	}
+
+	chart := map[string]int{}
+
+	for _, w := range regexp.MustCompile(`[\s\t\r\n]+`).Split(s, -1) {
+		chart[w]++
+	}
+
+	chartStats := []wordStats{}
+
+	for w, c := range chart {
+		chartStats = append(chartStats, wordStats{w, c})
+	}
+
+	sort.Slice(chartStats, func(i, j int) bool {
+		return chartStats[i].count > chartStats[j].count
+	})
+
+	var res []string
+
+	for i := 0; i < len(chartStats); i++ {
+		if i == topSize {
+			break
+		}
+
+		res = append(res, chartStats[i].word)
+	}
+
+	return res
 }
