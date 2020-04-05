fd44f85b1666620d05279fe3d9a8a50e3dd50a61
andrey.a.kor <info@andreykor.com>
2020-04-05 19:50:06 +0300

    HW4 is completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,22 +1,84 @@
 package hw04_lru_cache //nolint:golint,stylecheck
 
+import (
+	"sync"
+)
+
 type Key string
 
 type Cache interface {
-	// Place your code here
+	Set(key Key, value interface{}) bool // Добавить значение в кэш по ключу
+	Get(key Key) (interface{}, bool)     // Получить значение из кэша по ключу
+	Clear()                              // Очистить кэш
 }
 
 type lruCache struct {
-	// Place your code here:
-	// - capacity
-	// - queue
-	// - items
+	capacity int
+	queue    List
+	items    map[Key]*Item
+	mux      sync.Mutex
 }
 
 type cacheItem struct {
-	// Place your code here
+	key   Key
+	value interface{}
+}
+
+// Добавляет значение в кэш по ключу
+func (l *lruCache) Set(key Key, value interface{}) bool {
+	l.mux.Lock()
+	defer l.mux.Unlock()
+
+	if i, ok := l.items[key]; ok {
+		i.Value.(*cacheItem).value = value
+		l.queue.MoveToFront(i)
+
+		return true
+	}
+
+	i := l.queue.PushFront(&cacheItem{key, value})
+	l.queue.MoveToFront(i)
+	l.items[key] = i
+
+	if l.queue.Len() > l.capacity {
+		i := l.queue.Back()
+		l.queue.Remove(i)
+
+		delete(l.items, i.Value.(*cacheItem).key)
+	}
+
+	return false
+}
+
+// Возвращает значение из кэша по ключу
+func (l *lruCache) Get(key Key) (interface{}, bool) {
+	l.mux.Lock()
+	defer l.mux.Unlock()
+
+	if i, ok := l.items[key]; ok {
+		l.queue.MoveToFront(i)
+
+		return i.Value.(*cacheItem).value, true
+	}
+
+	return nil, false
+}
+
+// Очиститка кэша
+func (l *lruCache) Clear() {
+	l.mux.Lock()
+	defer l.mux.Unlock()
+
+	for key, i := range l.items {
+		l.queue.Remove(i)
+		delete(l.items, key)
+	}
 }
 
 func NewCache(capacity int) Cache {
-	return &lruCache{}
+	return &lruCache{
+		capacity: capacity,
+		queue:    NewList(),
+		items:    make(map[Key]*Item),
+	}
 }
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,15 +1,119 @@
 package hw04_lru_cache //nolint:golint,stylecheck
 
 type List interface {
-	// Place your code here
+	Len() int                      // длина списка
+	Front() *Item                  // первый Item
+	Back() *Item                   // последний Item
+	PushFront(v interface{}) *Item // добавить значение в начало
+	PushBack(v interface{}) *Item  // добавить значение в конец
+	Remove(i *Item)                // удалить элемент
+	MoveToFront(i *Item)           // переместить элемент в начало
 }
 
-type listItem struct {
-	// Place your code here
+type Item struct {
+	Value interface{} // значение
+	Next  *Item       // следующий элемент
+	Prev  *Item       // предыдущий элемент
 }
 
 type list struct {
-	// Place your code here
+	front *Item // Первый элемент списка
+	back  *Item // Последний элемент списка
+	size  int   // Размер списка
+}
+
+// Длина списка
+func (l *list) Len() int {
+	return l.size
+}
+
+// Первый элемент списка
+func (l *list) Front() *Item {
+	return l.front
+}
+
+// Последний элемент списка
+func (l *list) Back() *Item {
+	return l.back
+}
+
+// Добавляет значение в начало списка
+func (l *list) PushFront(v interface{}) *Item {
+	if l.front == nil {
+		l.front = &Item{
+			Value: v,
+		}
+		l.back = l.front
+	} else {
+		l.front.Next = &Item{
+			Value: v,
+			Prev:  l.front,
+		}
+
+		l.front = l.front.Next
+	}
+
+	l.size++
+
+	return l.front
+}
+
+// Добавляет значение в конец списка
+func (l *list) PushBack(v interface{}) *Item {
+	if l.back == nil {
+		l.back = &Item{
+			Value: v,
+		}
+		l.front = l.back
+	} else {
+		l.back.Prev = &Item{
+			Value: v,
+			Next:  l.back,
+		}
+
+		l.back = l.back.Prev
+	}
+
+	l.size++
+
+	return l.back
+}
+
+// Удаляет элемент из списка
+func (l *list) Remove(i *Item) {
+	if i == nil {
+		return
+	}
+
+	l.remove(i)
+	l.size--
+}
+
+func (l *list) remove(i *Item) {
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	} else {
+		l.back = i.Next
+	}
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	} else {
+		l.front = i.Prev
+	}
+}
+
+// переместить элемент в начало
+func (l *list) MoveToFront(i *Item) {
+	if i == nil || i == l.front {
+		return
+	}
+
+	l.remove(i)
+
+	l.front.Next = i
+	i.Prev = l.front
+	i.Next = nil
+	l.front = i
 }
 
 func NewList() List {
