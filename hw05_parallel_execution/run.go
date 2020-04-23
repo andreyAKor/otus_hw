9d6636d7291e8ef7ff3c46c3b9aebd54033255eb
andrey.a.kor <info@andreykor.com>
2020-04-23 21:29:59 +0300

    HW5 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -3,61 +3,12 @@ package hw05_parallel_execution //nolint:golint,stylecheck
 import (
 	"errors"
 	"sync"
+	"sync/atomic"
 )
 
-var (
-	ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
-
-	wg  *sync.WaitGroup = &sync.WaitGroup{}
-	mux *sync.Mutex     = &sync.Mutex{}
-
-	quitCh chan struct{}
-	taskCh chan Task
-
-	errs  int
-	errCh chan error
-
-	countTasks int
-)
+var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
-
-func worker(m int) {
-	defer wg.Done()
-
-	for {
-		select {
-		case <-quitCh:
-			return
-		default:
-		}
-
-		select {
-		case <-quitCh:
-			return
-		case task := <-taskCh:
-			if err := task(); err != nil {
-				mux.Lock()
-				errs++
-
-				if errs == m {
-					errCh <- ErrErrorsLimitExceeded
-				}
-
-				mux.Unlock()
-			}
-
-			mux.Lock()
-			countTasks--
-
-			if countTasks == 0 {
-				errCh <- nil
-			}
-
-			mux.Unlock()
-		}
-	}
-}
 
 // Run starts tasks in N goroutines and stops its work when receiving M errors from tasks
 func Run(tasks []Task, n int, m int) error {
@@ -65,34 +16,65 @@ func Run(tasks []Task, n int, m int) error {
 		return ErrErrorsLimitExceeded
 	}
 
-	countTasks = len(tasks)
-
-	errs = 0
-	errCh = make(chan error, 1)
-	defer close(errCh)
-
-	taskCh = make(chan Task)
-	defer close(taskCh)
-
-	defer wg.Wait()
-
-	quitCh = make(chan struct{})
-	defer close(quitCh)
+	wg := &sync.WaitGroup{}
+	errs := int64(0)
+	taskCh := make(chan Task)
+	doneCh := make(chan struct{})
 
 	wg.Add(n)
 	for i := 0; i < n; i++ {
-		go worker(m)
+		go worker(m, wg, doneCh, taskCh, &errs)
 	}
 
 	for _, task := range tasks {
 		select {
-		case err := <-errCh:
-			return err
+		case <-doneCh:
+			return ErrErrorsLimitExceeded
+		case taskCh <- task:
+		}
+	}
+
+	close(taskCh)
+	wg.Wait()
+
+	select {
+	case <-doneCh:
+		return ErrErrorsLimitExceeded
+	default:
+	}
+
+	return nil
+}
+
+func worker(
+	m int,
+	wg *sync.WaitGroup,
+	doneCh chan struct{},
+	taskCh chan Task,
+	errs *int64,
+) {
+	defer wg.Done()
+
+	for {
+		select {
+		case <-doneCh:
+			return
 		default:
 		}
 
-		taskCh <- task
-	}
+		select {
+		case <-doneCh:
+			return
+		case task, ok := <-taskCh:
+			if !ok {
+				return
+			}
 
-	return <-errCh
+			if err := task(); err != nil {
+				if atomic.AddInt64(errs, 1) == int64(m) {
+					close(doneCh)
+				}
+			}
+		}
+	}
 }
