8782ff3556f12b2300e626c6d7e1454a5e61a12f
andrey.a.kor <info@andreykor.com>
2020-05-11 11:31:16 +0300

    HW7 is completed

--- hw06_pipeline_execution/go.mod
+++ hw06_pipeline_execution/go.mod
@@ -1,7 +1,5 @@
-module github.com/andreyAKor/otus_hw/hw06_pipeline_execution
+module github.com/fixme_my_friend/hw06_pipeline_execution
 
 go 1.14
 
-require (
-	github.com/stretchr/testify v1.5.1
-)
+require github.com/stretchr/testify v1.5.1
--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -10,36 +10,6 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	out := make(Bi)
-
-	switch len(stages) {
-	case 0:
-		defer close(out)
-		return out
-	case 1:
-		return stages[0](in)
-	}
-
-	go func() {
-		defer close(out)
-
-		stream := in
-		for _, stage := range stages {
-			stream = stage(stream)
-		}
-
-		for {
-			select {
-			case <-done:
-				return
-			case i, ok := <-stream:
-				if !ok {
-					return
-				}
-				out <- i
-			}
-		}
-	}()
-
-	return out
+	// Place your code here
+	return nil
 }
