e0a6345e5ca2037d53807a0f8b6b5e05a9c4eb7e
andrey.a.kor <info@andreykor.com>
2020-05-26 09:08:11 +0300

    HW8 is completed

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,104 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const envDir = "./testdata/env"
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	envDir := "./testdata/env"
+
+	t.Run("empty dir", func(t *testing.T) {
+		_, err := ReadDir("")
+
+		require.Error(t, err)
+	})
+
+	t.Run("file with \\n", func(t *testing.T) {
+		tmpfile, err := ioutil.TempFile(envDir, "test")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer os.Remove(tmpfile.Name())
+
+		if _, err := tmpfile.Write([]byte("\n")); err != nil {
+			t.Fatal(err)
+		}
+
+		fi, err := tmpfile.Stat()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if err := tmpfile.Close(); err != nil {
+			t.Fatal(err)
+		}
+
+		env, err := ReadDir(envDir)
+		require.NoError(t, err)
+
+		v, ok := env[fi.Name()]
+		require.True(t, ok)
+		require.Equal(t, "", v)
+	})
+
+	t.Run("file with zero byte", func(t *testing.T) {
+		tmpfile, err := ioutil.TempFile(envDir, "test")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer os.Remove(tmpfile.Name())
+
+		if _, err := tmpfile.Write([]byte{0}); err != nil {
+			t.Fatal(err)
+		}
+
+		fi, err := tmpfile.Stat()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if err := tmpfile.Close(); err != nil {
+			t.Fatal(err)
+		}
+
+		env, err := ReadDir(envDir)
+		require.NoError(t, err)
+
+		v, ok := env[fi.Name()]
+		require.True(t, ok)
+		require.Equal(t, "\n", v)
+	})
+
+	t.Run("filename with =", func(t *testing.T) {
+		tmpfile, err := ioutil.TempFile(envDir, "test=")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer os.Remove(tmpfile.Name())
+
+		if _, err := tmpfile.Write([]byte("lala")); err != nil {
+			t.Fatal(err)
+		}
+
+		fi, err := tmpfile.Stat()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if err := tmpfile.Close(); err != nil {
+			t.Fatal(err)
+		}
+
+		env, err := ReadDir(envDir)
+		require.NoError(t, err)
+
+		_, ok := env[fi.Name()]
+		require.False(t, ok)
+	})
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,24 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("return 0", func(t *testing.T) {
+		r := RunCmd([]string{"env", "-i"}, Environment{})
+		require.Equal(t, 0, r)
+	})
+
+	t.Run("run unknow command", func(t *testing.T) {
+		r := RunCmd([]string{"abracadabra"}, Environment{})
+		require.Equal(t, -1, r)
+	})
+
+	t.Run("run command without params", func(t *testing.T) {
+		r := RunCmd([]string{"cp"}, Environment{})
+		require.Equal(t, 1, r)
+	})
 }
