816e4135fdc363b67ec73e4100ce04004e3ffd1d
andrey.a.kor <info@andreykor.com>
2020-06-01 15:24:32 +0300

    HW11 is completed

--- hw11_telnet_client/go.mod
+++ hw11_telnet_client/go.mod
@@ -1,5 +1,9 @@
-module github.com/fixme_my_friend/hw11_telnet_client
+module github.com/andreyAKor/otus_hw/hw11_telnet_client
 
 go 1.14
 
-require github.com/stretchr/testify v1.5.1
+require (
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/stretchr/testify v1.5.1
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+)
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,18 +1,85 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"io"
+	"net"
 	"time"
 )
 
+var (
+	ErrConnClose = errors.New("...Connection was closed by peer")
+	ErrEOF       = errors.New("...EOF")
+)
+
 type TelnetClient interface {
-	// Place your code here
+	Connect() error
+	Send() error
+	Receive() error
+	Close() error
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here
-	return nil
+func NewTelnetClient(
+	address string,
+	timeout time.Duration,
+	in io.ReadCloser,
+	out io.Writer,
+) TelnetClient {
+	return &Telnet{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
 }
 
-// Place your code here
-// P.S. Author's solution takes no more than 50 lines
+type Telnet struct {
+	address     string
+	timeout     time.Duration
+	in          io.ReadCloser
+	out         io.Writer
+	conn        net.Conn
+	connScanner *bufio.Scanner
+	inScanner   *bufio.Scanner
+}
+
+func (t *Telnet) Connect() (err error) {
+	t.conn, err = net.DialTimeout("tcp", t.address, t.timeout)
+
+	t.connScanner = bufio.NewScanner(t.conn)
+	t.inScanner = bufio.NewScanner(t.in)
+
+	return
+}
+
+func (t *Telnet) Receive() (err error) {
+	if t.conn == nil {
+		return
+	}
+	if !t.connScanner.Scan() {
+		return ErrConnClose
+	}
+	_, err = t.out.Write([]byte(t.connScanner.Text() + "\n"))
+	return
+}
+
+func (t *Telnet) Send() (err error) {
+	if t.conn == nil {
+		return
+	}
+	if !t.inScanner.Scan() {
+		return ErrEOF
+	}
+	_, err = t.conn.Write([]byte(t.inScanner.Text() + "\n"))
+	return
+}
+
+func (t *Telnet) Close() (err error) {
+	if t.conn != nil {
+		err = t.conn.Close()
+	}
+	return
+}
+
+var _ TelnetClient = (*Telnet)(nil)
